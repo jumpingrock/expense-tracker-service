d1fd7b22f29f708d7611c94d2b0f92f6d8342733
jumpingrock <kennethyeongky@gmail.com>
2021-05-03 02:20:39 +0800

    add expense controller to app

--- src/app/controllers.ts
+++ src/app/controllers.ts
@@ -1,9 +1,11 @@
 import { VersionController } from './version/VersionController';
 import { UserController } from './user/controller/UserController';
 import { BudgetController } from './budget/controller/BudgetController';
+import { ExpenseController } from './expense/controller/ExpenseController';
 
 export const controllers = [
   VersionController,
   UserController,
-  BudgetController
+  BudgetController,
+  ExpenseController
 ];
