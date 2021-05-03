c4476ba529f33874bf5e121f2b0b154c27282f8a
jumpingrock <kennethyeongky@gmail.com>
2021-05-03 23:37:37 +0800

    added update and delete for expense service

--- src/app/expense/controller/ExpenseController.ts
+++ src/app/expense/controller/ExpenseController.ts
@@ -1,4 +1,14 @@
-import { Authorized, Body, CurrentUser, JsonController, OnUndefined, Post } from 'routing-controllers';
+import {
+  Authorized,
+  Body,
+  CurrentUser,
+  Delete,
+  Get,
+  JsonController,
+  OnUndefined,
+  Param, Patch,
+  Post,
+} from 'routing-controllers';
 import { ExpenseRequest } from './ExpenseRequest';
 import { ExpenseService } from '../service/ExpenseService';
 import { Expense } from '../Expense';
@@ -10,8 +20,35 @@ export class ExpenseController {
   @OnUndefined(201)
   async createExpense(
   @CurrentUser() userId,
-  @Body() requestParam: ExpenseRequest,
-  ): Promise<Expense> {
+  @Body() requestParam: ExpenseRequest,): Promise<Expense> {
     return await new ExpenseService().createNewExpense(userId, requestParam)
+  }
+
+  @Authorized()
+  @Get('/budgetId/:budget_by_id')
+  @OnUndefined(201)
+  async getExpensesByBudget(
+    @CurrentUser() userId: number,
+    @Param('budget_by_id') budgetById: number): Promise<Expense[]> {
+    return await new ExpenseService().getExpensesByBudgetById(userId, budgetById)
+  }
+
+  @Authorized()
+  @Delete('/expenseId/:expense_id')
+  @OnUndefined(201)
+  async deleteExpense(
+    @CurrentUser() userId: number,
+    @Param('expense_id') expenseId: number): Promise<any> {
+    return await new ExpenseService().deleteExpenseById(userId, expenseId)
+  }
+
+  @Authorized()
+  @Patch('/expenseId/:expense_id')
+  @OnUndefined(201)
+  async updateExpense(
+    @CurrentUser() userId: number,
+    @Param('expense_id') expenseId: number,
+    @Body() requestParam: ExpenseRequest,): Promise<any> {
+    return await new ExpenseService().updateExpenseById(userId, requestParam, expenseId)
   }
 }
--- src/app/expense/service/ExpenseService.ts
+++ src/app/expense/service/ExpenseService.ts
@@ -27,4 +27,26 @@ export class ExpenseService {
 
     return ExpenseMapper(expenseDBResp)
   }
+
+  async getExpensesByBudgetById (userId: number, budgetById: number): Promise<Expense[]> {
+    const expenses: any = await ExpenseDBModel
+      .findAll({ where: { budgetById: budgetById }, order: [['createdAt', 'DESC']], })
+    return expenses.map((expense) => ExpenseMapper(expense))
+  }
+
+  async deleteExpenseById (userId: number, expenseId: number): Promise<any> {
+    return await ExpenseDBModel.destroy({ where: { id: expenseId, createdById: userId } })
+  }
+
+  async updateExpenseById (userId: number, expenseObj: ExpenseRequest, expenseId: number): Promise<Expense> {
+    const sequelize = DB.getInstance()
+    await sequelize.transaction(async (transaction) => {
+      await ExpenseDBModel.update({
+        creditOrDebit: expenseObj.creditOrDebit,
+        amount: expenseObj.amount
+      }, { where: { id: expenseId, createdById: userId } })
+    })
+    const expenseDBResp: any = await ExpenseDBModel.findByPk(expenseId)
+    return ExpenseMapper(expenseDBResp)
+  }
 }
